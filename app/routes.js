2807d8818c475a5e892321e269095564dc0d228c
Dronic Ion <iondronic@gmail.com>
2017-02-07 01:34:14 +0100

    first commit

---
+++ app/routes.js
@@ -0,0 +1,29 @@
+// app/routes.js
+module.exports = function(app){
+  app.get('/', function(req, res){
+    res.render('index.ejs');
+  });
+
+  app.post('/subscribe', function(req, res){
+    var results= [];
+
+    //grab data from http request
+    var data = {email: req.body.email};
+    var connectionString = 'postgres://ifmdaskjdzmyci:aR5F-iCwhIlH4FVXf1XBQkJTdW@ec2-54-228-219-2.eu-west-1.compute.amazonaws.com:5432/d5md0il57415vo';
+    var pq = require('pg');
+    pg.defaults.ssl = true;
+    pg.connect(connectionString,
+      function(err, client, done){
+        if(err){
+          done();
+          console.log(err);
+          return res.status(500).json({success: false, data:err});
+        }
+        var insertQuery = client.query("INSERT INTO dronic_users(email) values($1)", [data.email]);
+        insertQuery.on('end', function(){
+          done();
+          return res.redirect('/');
+        });
+    });
+  });
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+//server.js
+//get the tools we need
+
+var express = require('express');
+var app = express();
+
+//require our database connection
+var pg = require('pg');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(flash()); //use connect-flash for flash messages stored in session
+
+require('./app/routes.js')(app);
+
+app.use(express.static(__dirname + "/public"));
+var port = process.env.PORT || 3000;
+
+//launch =========
+app.listen(port);
+console.log('Dronic happens on port '+ port);
