85a1079f7f020f452efd7118d925b569adb2c142
Dronic Ion <iondronic@gmail.com>
2017-02-21 22:15:49 +0100

    added privacy policy page

--- app/routes.js
+++ app/routes.js
@@ -3,16 +3,16 @@ require('dotenv').config({silent: true});
 
 module.exports =  (app) => {
 
-    app.get('/', (req, res) => {
-        res.render('index.ejs');
-    });
-    app.get('/chat', (req, res) => {
-        res.render('chat.ejs');
-    });
-
     //free ssl encryption
     app.get('/.well-known/acme-challenge/:content', (req, res) => {
         res.send(process.env.SSL_SECRET);
     });
 
+    app.get('/', (req, res) => {
+        res.render('index.ejs');
+    });
+
+    app.get('/privacy-policy/ ', (req,res) => {
+        res.send('privacy.ejs');
+    });
 }
