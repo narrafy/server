5c5f3e0af7cfcb466282227dd79ec5ab915132f6
Dronic Ion <iondronic@gmail.com>
2017-02-23 02:55:24 +0100

    fb hm... fix

--- app/watson.js
+++ app/watson.js
@@ -97,14 +97,15 @@ function facebookRequest(facebook, err, result){
             if (data.output.text) {
                 facebook.log(facebook.data.id, data);
                 //watson have an answer
-                if(data.output.text[0]){
-                    if(facebook){
-                        facebook.message(facebook.data.id, data.output.text[0]);
-                    }
-                }else if(data.output.text.length>=1)
-                {
+                if( data.output.text.length>=1){
                     if(facebook){
                         facebook.message(facebook.data.id, data.output.text[0] +' '+ data.output.text[1]);
+
+                    }
+                }else if(data.output.text[0])
+                {
+                    if(facebook){
+                        facebook.message(facebook.data.id, data.output.text[0]);
                     }
                 }
             }
