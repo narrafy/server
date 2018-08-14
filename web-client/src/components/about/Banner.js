62224e95791579d12924a82ecb777babf0a0e7af
Dronic Ion <iondronic@gmail.com>
2018-08-14 15:32:36 +0200

    runscripts in heroku

--- web-client/src/components/about/Banner.js
+++ web-client/src/components/about/Banner.js
@@ -9,8 +9,9 @@ function Banner()
                     <div className="col-lg-10 m-auto">
                         <div className="block">
                             <h1>About Us</h1>
-                            <h3> We are building a self-learning conversational agent to help people better
-                                understand themselves. <br/>
+                            <h3> We are building machine learning tools to streamline repetitive processes in the
+                                 therapy rooms around the world.
+                                <br/>
                             </h3>
                             <div className="download">
                                 <a href="https://arxiv.org/abs/1712.03080" className="btn btn-default">
