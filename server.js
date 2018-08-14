0fe1e8b8924050521b8a26fbe3742d2a949b4cdc
Dronic Ion <iondronic@gmail.com>
2018-08-14 13:07:19 +0200

    heroku integration 7

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ require('./app/modules/router/routes.js')(app, db)
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'web-client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/web-client/build/index.html'));
 });
 
 /* Connect to DB */
